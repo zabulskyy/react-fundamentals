e210875b22120917688ee7855c87030d17eadc71
zabulskyy <v.zabulskyy@gmail.com>
2017-08-24 15:57:45 +0300

    auth system

--- src/modules/Auth/components/hasAuth/index.js
+++ src/modules/Auth/components/hasAuth/index.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Route, Redirect, withRouter } from 'react-router';
+import { Route, withRouter } from 'react-router';
 import withAuthHoC from '../../HoCs/withAuth';
 
 const hasAuth = WrappedComponent => class hasAuth extends Component {
   static propTypes = {
-    match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
     user: PropTypes.object,
   }
 
   render() {
-    const { match, location, history, user } = this.props
-
     return (
       <Route exact path="/" render={() => (
-        user ? (
-          <WrappedComponent {...this.props} user={this.props.user} />;
+        this.props.user ? (
+          <WrappedComponent {...this.props} />;
         ) : (
           <Redirect to="/login"/>
         )
@@ -26,8 +21,4 @@ const hasAuth = WrappedComponent => class hasAuth extends Component {
   }
 }
 
-hasAuth.propTypes = {
-  user: PropTypes.object,
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withAuthHoC(hasAuth)));
+export default withAuthHoC(hasAuth);
